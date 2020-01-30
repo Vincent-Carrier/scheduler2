da9de2e13d6368c9d557ffbabe77508c8f5c4aa4
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-30 15:10:10 -0500

    Write first Cypress tests

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,10 @@
+describe("Appointment", () => {
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("Monday");
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+  });
+});
